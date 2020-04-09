dc9564639baabc67abc83c9c658ee596cd5be802
Andrew Wheeler <andrew@smallbytes.app>
2020-04-08 20:38:49 -0500

    Added Card and Board

---
+++ src/Board/Board.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "../Card/Card";
+
+const Board = () => {
+  // displays 8 cards
+  return (
+    <div>
+      <Card number="1" />
+      <Card number="1" />
+      <Card number="1" />
+      <Card number="1" />
+      <br />
+      <Card number="1" />
+      <Card number="1" />
+      <Card number="1" />
+      <Card number="1" />
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/Card/Card.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Card.css";
+
+const Card = ({ number }) => {
+  return <button>{number}</button>;
+};
+
+export default Card;
